9d246186f3ede67d3624dd7c60df99bf1b34461e
sasugadayo <sasugaff@icloud.com>
2020-03-03 18:44:08 +0900

    基礎構築

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,6 +2,9 @@ class Shop < ApplicationRecord
   validates :text, presence: true
   belongs_to :user
   has_many :comments  # commentsテーブルとのアソシエーション
+  has_many :favorites
+  has_many :users, through: :favorites
+
 
   def self.search(search)
     return Shop.all unless search
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'favorites/create'
+  get 'favorites/destroy'
   devise_for :users
   root to: 'shops#index'
   resources :shops do
