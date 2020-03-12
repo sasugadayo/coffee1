acd30092488eada7b17df03500fae7689ecf8ab4
sasugadayo <sasugaff@icloud.com>
2020-03-12 12:33:30 +0900

    自動デプロイ修正２

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.12.1'
 set :application, 'coffee1'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:sasugadayo/coffeea.git'
+set :repo_url,  'git@github.com:sasugadayo/coffee1.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
